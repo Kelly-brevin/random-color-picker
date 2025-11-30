f4b0906330460f8160d435fcb5b5993373788de8
kelly brevin <kellybrevin83@gmail.com>
2025-11-30 20:08:36 +0300

    style changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//create a placeholder that prompts the user to click.
 class DisplayEl {
   constructor() {
     this.hexValue = document.getElementById("hex-value");
@@ -8,9 +9,12 @@ class DisplayEl {
       this.hexValue.innerText = "Press the spacebar";
     }
   }
+
+  //check if the user has clicked the spacebar and remove the placeholder, replace with hex value
 }
 //create an instance
 const displayEl = new DisplayEl();
 //cal
 
 //call the method
+displayEl.placeholder();
