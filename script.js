478a7e3a85e4c7b91b75f9b691e209f5ac1d8d6a
kelly brevin <kellybrevin83@gmail.com>
2025-12-01 23:43:23 +0300

    logic

--- script.js
+++ script.js
@@ -13,8 +13,13 @@ class DisplayEl {
   //check if the user has clicked the spacebar and remove the placeholder.
   //listener is set to the whole page. An event object will be returned with info as to what key was pressed
   clickEvent() {
-    document.addEventListener("keydown", (e) => {});
+    document.addEventListener("keydown", (e) => {
+      if (e.key === " ") {
+        //call the method with the random color logic
+      }
+    });
   }
+  randomColor() {}
 }
 //Logic to generate  random HEX values,store them in a variable
 
