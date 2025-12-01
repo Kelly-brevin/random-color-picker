3b99ab90cb7b996a018f61ca7cee1872803f92d3
kelly brevin <kellybrevin83@gmail.com>
2025-12-01 23:32:07 +0300

    event listener for keyboard

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ class DisplayEl {
     this.hexValue = document.getElementById("hex-value");
   }
 
-  placeholder() {
+  placeHolder() {
     if (!this.hexValue.innerText.trim()) {
       this.hexValue.innerText = "Press the spacebar";
     }
   }
 
-  //check if the user has clicked the spacebar and remove the placeholder, replace with hex value
+  //check if the user has clicked the spacebar and remove the placeholder.
+  //listener is set to the whole page. An event object will be returned with info as to what key was pressed
+  clickEvent() {
+    document.addEventListener("keydown", (e) => {});
+  }
 }
-//create an instance
-const displayEl = new DisplayEl();
+//Logic to generate  random HEX values,store them in a variable
+
+// const displayEl = new DisplayEl();
 
 //call the method
-displayEl.placeholder();
+// displayEl.placeHolder();
