24adde0cae6fdeca488d265ccc1149259b41b6ec
kelly brevin <kellybrevin83@gmail.com>
2025-12-02 11:00:40 +0300

    logic changes

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-//create a placeholder that prompts the user to click.
 class DisplayEl {
   constructor() {
     this.hexValue = document.getElementById("hex-value");
   }
-
+  //create a placeholder that prompts the user to click.
   placeHolder() {
     if (!this.hexValue.innerText.trim()) {
       this.hexValue.innerText = "Press the spacebar";
@@ -14,16 +13,23 @@ class DisplayEl {
   //listener is set to the whole page. An event object will be returned with info as to what key was pressed
   clickEvent() {
     document.addEventListener("keydown", (e) => {
-      if (e.key === " ") {
+      if (e.key === "") {
         //call the method with the random color logic
       }
     });
   }
-  randomColor() {}
+  randomColor() {
+    const values = [];
+    for (let i = 0; i < 3; i++) {
+      values.push(Math.floor(Math.random() * 255));
+    }
+    console.log(values);
+  }
 }
+
 //Logic to generate  random HEX values,store them in a variable
 
-// const displayEl = new DisplayEl();
+const displayEl = new DisplayEl();
 
 //call the method
-// displayEl.placeHolder();
+displayEl.randomColor();
